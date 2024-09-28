ae86d5c928adaf122692d7f898ecbd3839815754
@VirusLabItalia <145903078+VirusLabIT@users.noreply.github.com>
2024-09-28 09:18:09 +0200

    AddedCustomTheme

--- installation.sh
+++ installation.sh
@@ -1,14 +1,30 @@
 git clone https://github.com/VirusLabIT/homer_viruslab.git
 cd homer_viruslab
-docker-compose up -d
-if [-d /assets ]
+
+packagesNeeded=docker-compose-plugin
+if [ -x "$(command -v apt-get)" ];
+then
+    sudo apt-get install "${packagesNeeded[@]}"
+elif [ -x "$(command -v yum)" ];
+then
+    sudo yum install "${packagesNeeded[@]}"
+elif [ -x "$(command -v pamac)" ];
+then
+    sudo pamac install docker-compose
+else
+    echo "FAILED TO INSTALL PACKAGE: You must install: "${packagesNeeded[@]}" manually">&2;
+    return
+fi
+
+docker compose up -d
+if [ -d /assets ]
 then
     echo "backuping /assets"
-    mv /assets /assets.Backup
-    mv assets /assets
+    sudo mv /assets /assets.Backup
+    sudo mv assets /assets
 else
     echo "assets not present"
     echo "creating assets"
-    mv assets /assets
+    sudo mv assets /assets
 fi
-echo "installed"
+sudo chown  1000:1000 /assets
