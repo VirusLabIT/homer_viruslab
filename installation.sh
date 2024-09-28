46ddc0568645e879e3452d57134db3ce4c96dde6
@VirusLabItalia <145903078+VirusLabIT@users.noreply.github.com>
2024-09-28 11:03:08 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,7 +1,11 @@
-git clone https://github.com/VirusLabIT/homer_viruslab.git
+  GNU nano 7.2                                         installation.sh
+if [ ! -d /homer_viruslab ]
+then
+    git clone https://github.com/VirusLabIT/homer_viruslab.git
+fi
 cd homer_viruslab
 docker-compose up -d
-if [ -d /assets ];
+if [ ! -d /assets ]
 then
     echo "backuping /assets"
     mv /assets /assets.Backup
@@ -10,4 +14,5 @@ else
     echo "assets not present"
     echo "creating assets"
     mv assets /assets
-    fi
+fi
+echo "installed"
